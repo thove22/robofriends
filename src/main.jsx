9255b8065219483aa0796e639a9eb46f78dee4d5
thove22 <pedrostovela@gmail.com>
2025-03-03 13:24:32 -0800

    Criando cards dinamicos

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
+import 'tachyons'
+import { robots } from './robots'
+import Card from './Card'
+ 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <h1>Hello World</h1>
-    <App/>
-   </StrictMode>,
+    <div>
+        <Card id={robots[0].id} name={robots[0].name} email= {robots[0].email}/>
+        <Card id={robots[1].id} name={robots[1].name} email= {robots[1].email}/>
+        <Card id={robots[2].id} name={robots[2].name} email= {robots[2].email}/>
+    </div> 
 )
