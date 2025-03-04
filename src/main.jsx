86cb0bfd15e14357dd41df6cd6115bcdeda3a27f
thove22 <pedrostovela@gmail.com>
2025-03-04 06:38:49 -0800

    Adicionando a funcionalidade para exibir todas cards

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'tachyons'
+import CardList from './CardList'
 import { robots } from './robots'
-import Card from './Card'
+ 
  
 createRoot(document.getElementById('root')).render(
-    <div>
-        <Card id={robots[0].id} name={robots[0].name} email= {robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email= {robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email= {robots[2].email}/>
-    </div> 
+    <CardList robots = {robots} />
 )
